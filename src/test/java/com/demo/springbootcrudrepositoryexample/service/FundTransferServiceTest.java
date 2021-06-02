0b2e785f89bacf2010f63606f4012cedf5995830
gudaviswanatha.r <gudaviswanatha.r@LP1-AP-51722337.PROD.HCLPNP.COM>
2021-06-02 16:13:26 +0530

    updated junits

--- src/test/java/com/demo/springbootcrudrepositoryexample/service/FundTransferServiceTest.java
+++ src/test/java/com/demo/springbootcrudrepositoryexample/service/FundTransferServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.demo.springbootcrudrepositoryexample.entity.FundTransferDetails;
 import com.demo.springbootcrudrepositoryexample.entity.MainAccounts;
+import com.demo.springbootcrudrepositoryexample.entity.RelatedAccount;
 @ExtendWith(MockitoExtension.class)
 class FundTransferServiceTest {
 	@InjectMocks
@@ -26,7 +27,7 @@ class FundTransferServiceTest {
 	  
 	
 	@Test
-	@DisplayName("FundTransfer:Positive Scenario")
+	@DisplayName("FundTransfer:MainAccNotExist")
 	void testTransferFund() {
 		
 		 FundTransferDetails fundTransferDetails = new FundTransferDetails();
@@ -41,8 +42,58 @@ class FundTransferServiceTest {
 		Assertions.assertThat(transferFund).isEqualTo("Invalid Main Account");
 	}
 	
+	@Test
+	void testTransferFund_Success() {
+		
+		 FundTransferDetails fundTransferDetails = new FundTransferDetails();
+		 fundTransferDetails.setAmount(3000L);
+		 fundTransferDetails.setMainAccountNumber(123L);
+		 fundTransferDetails.setRelatedAccountNumber(111L);
+		 Mockito.when(relatedAccountRepository.findByMainAccountLink(Mockito.anyLong())).thenReturn(123L);
+		 MainAccounts val = new MainAccounts();
+		 val.setAccountNumber(123L);
+		 val.setName("VISHH");
+		 val.setBalance(565465L);
+		 RelatedAccount val1 = new RelatedAccount();
+		 val.setAccountNumber(111L);
+		 val.setName("VISHH");
+		 val.setBalance(565465L);
+		Mockito.when(mainAccountRepository
+					.findByAccountNumber(Mockito.anyLong())).thenReturn(Optional.of(val) );
+		Mockito.when(relatedAccountRepository.findByAccountNumber(Mockito.anyLong())).thenReturn(Optional.of(val1) );
+		Mockito.when(mainAccountRepository.getBalanceMainAccount(Mockito.anyLong())).thenReturn(6000L);
+		Mockito.when(relatedAccountRepository.getBalanceRelatedAccount(Mockito.anyLong())).thenReturn(111L);
+		Mockito.doNothing()
+		.when(mainAccountRepository)
+		.updateMainAccountBalance(Mockito.anyLong(),Mockito.anyLong()); 
+		Mockito.doNothing()
+		.when(relatedAccountRepository).updateRelatedAccountBalance(Mockito.anyLong(),Mockito.anyLong()); 
+		String transferFund = fundTransferService.transferFund(fundTransferDetails );
+		Assertions.assertThat(transferFund).isEqualTo("Updated the balance in Related Account");
+	}
+	
+	@Test
+	void testTransferFund_RelatedAccNotExist() {
+		
+		 FundTransferDetails fundTransferDetails = new FundTransferDetails();
+		 fundTransferDetails.setAmount(3000L);
+		 fundTransferDetails.setMainAccountNumber(123L);
+		 fundTransferDetails.setRelatedAccountNumber(111L);
+		 Mockito.when(relatedAccountRepository.findByMainAccountLink(Mockito.anyLong())).thenReturn(123L);
+		 MainAccounts val = new MainAccounts();
+		 val.setAccountNumber(123L);
+		 val.setName("VISHH");
+		 val.setBalance(565465L);
+		 Optional<RelatedAccount> val1 = Optional.empty();
+		 Mockito.when(relatedAccountRepository.findByAccountNumber(Mockito.anyLong())).thenReturn(val1);
+		
+		 String transferFund = fundTransferService.transferFund(fundTransferDetails );
+		 Assertions.assertThat(transferFund).isEqualTo("Invalid Related Account");
+	}
+	
+	
 	/*
-	 * @Test
+	 * @Test8
 	 * 
 	 * @DisplayName("Save MainAccount") void testsaveMainAccountDetails() {
 	 * 
